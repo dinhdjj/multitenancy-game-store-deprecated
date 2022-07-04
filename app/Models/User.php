fc2f927957c38583b339358288d5d117d81f3d81
dinhdjj <dinhdjj@gmail.com>
2022-07-04 10:35:47 +0700

    chore: fix phpstan

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int,string>
      */
     protected $hidden = [
         'password',
@@ -45,7 +45,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array
+     * @var array<string,string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
